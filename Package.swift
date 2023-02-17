e6938841d03328305f0f8f048f83101f44e313c9
Build Er <builder@macmini6.local>
2023-02-16 23:49:58 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXCoreModule_SD_CardScanner",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXCoreModule_SD_CardScanner",
+			targets: ["GXCoreModule_SD_CardScannerWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", branch: "beta"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", branch: "beta"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", branch: "beta")
+	],
+	targets: [
+		.target(name: "GXCoreModule_SD_CardScannerWrapper",
+				dependencies: [
+					"GXCoreModule_SD_CardScanner",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.watchOS, .tvOS, .iOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXCoreModule_SD_CardScanner",
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_CardScanner-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "8a49af3d0a4c94e3a6f4a0fad4bf63c7590b5859735a1f954dde1b8006f1bddf"
+		)
+	]
+)
