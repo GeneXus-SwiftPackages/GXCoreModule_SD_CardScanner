85fd366a34f2fb159a24f3ef6ff7d90d59ed6896
genexusbot <builder@genexus.com>
2023-03-28 14:17:04 -0300

    Package version: 1.0.0-beta+20230328171001

--- Package.swift
+++ Package.swift
@@ -18,15 +18,15 @@ let package = Package(
 		.target(name: "GXCoreModule_SD_CardScannerWrapper",
 				dependencies: [
 					"GXCoreModule_SD_CardScanner",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .tvOS, .watchOS])),
 					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
 					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_CardScanner",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_CardScanner-1.0.0-beta+20230328162416.xcframework.zip",
-			checksum: "9b3e46d8efa6d2db769cd74bb730cbcc1d757d7a6e30dd7a9164f76dc80afa1f"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_CardScanner-1.0.0-beta+20230328171001.xcframework.zip",
+			checksum: "279a5a45d3c2b1c795ab68fcfa682581575cbc4217be52b6132029931b272309"
 		)
 	]
 )
